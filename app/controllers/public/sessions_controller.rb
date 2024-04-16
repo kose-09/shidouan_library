2310e944a427b2f8f5301ec78aa2a4b518a45b48
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-04-16 01:48:41 +0000

    [Update] 退会確認画面を修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :user_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,4 +25,15 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+
+  def user_state
+    user = User.find_by(email: params[:user][:email])
+    return if user.nil?
+    return unless user.valid_password?(params[:user][:password])
+    return if user.is_active == true
+    redirect_to new_user_registration_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   
-  get '/users/:id/confirm_withdrawal' => 'users#confirm_withdrawal', as: 'confirm_withdrawal'
-  patch '/users/:id/withdrawal' => 'users#withdrawal', as: 'withdrawal'
+  get '/users/:id/confirm_withdrawal' => 'public/users#confirm_withdrawal', as: 'confirm_withdrawal'
+  patch '/users/:id/withdrawal' => 'public/users#withdrawal', as: 'withdrawal'
   resources :users, only: [:show, :edit, :update], controller: 'public/users'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
