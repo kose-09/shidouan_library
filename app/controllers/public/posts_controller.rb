3af202e9457115bc7ba13e4fa1d2c98ff7e76f9b
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-04-16 15:11:23 +0000

    [Add] 新規投稿画面を作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,23 +1,36 @@
 class Public::PostsController < ApplicationController
   def index
+    @posts = Post.all
   end 
   
   def show
+    @post = Post.find(params[:id])
   end 
   
   def new
+    @post = Post.new
   end 
   
   def create
+    @post = Post.new
+    @post.save
+    redirect_to post_path(@post.id)
   end 
   
   def edit
+    @post = Post.find(params[:id])
   end 
   
   def update
+    @post = Post.find(params[:id])
+    @post.update
+    redirect_to post_path(@post.id)
   end 
   
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end 
   
   def pdf
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   get '/users/:id/confirm_withdrawal' => 'public/users#confirm_withdrawal', as: 'confirm_withdrawal'
   patch '/users/:id/withdrawal' => 'public/users#withdrawal', as: 'withdrawal'
   resources :users, only: [:show, :edit, :update], controller: 'public/users'
+  
+  resources :posts, controller: 'public/posts'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
