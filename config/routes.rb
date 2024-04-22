95fb6758bff62d47dc245ebce8b8526bf5b95e94
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-04-22 15:09:36 +0000

    [Add] post_commentsコントローラーのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,12 @@ Rails.application.routes.draw do
 
   resources :posts, controller: 'public/posts' do
     resource :favorite, only: [:create, :destroy], controller: 'public/favorites'
+    resources :post_comments, only: [:index, :create, :destroy], controller: 'public/post_comments'
   end
 
   resources :subject_categories, only: [:index, :create, :edit, :update], controller: 'admin/subject_categories'
-  
- 
+
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
