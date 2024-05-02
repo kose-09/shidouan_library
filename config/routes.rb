f65f79222bfcee68267111adb56368aaa3aa9793
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-05-02 14:48:44 +0000

    [Add] ゲストログイン機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,10 @@ Rails.application.routes.draw do
   end
 
   resources :subject_categories, only: [:index, :create, :edit, :update], controller: 'admin/subject_categories'
+  
+  devise_scope :user do
+    post "users/guest_sign_in", to: "public/sessions#guest_sign_in"
+  end
 
 
 
