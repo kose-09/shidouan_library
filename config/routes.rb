5a280019ca5178f51b5d4c117b4ac8d184f17db1
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-05-04 16:07:41 +0000

    [Add] 管理者用のpost_commentsコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users, only: [:index, :show, :edit, :update]
-    resources :posts, only: [:index, :show, :destroy]
+    resources :posts, only: [:index, :show, :destroy] do 
+      resources :post_comments, only: [:index, :destroy]
+    end 
     resources :subject_categories, only: [:index, :create, :edit, :update]
   end
   root to: 'public/homes#about'
