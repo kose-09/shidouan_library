901962032b7435863478f1fd105aa1acc2f61b44
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-05-04 14:36:58 +0000

    [Add] 管理者用のユーザー情報編集機能を追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,5 +9,23 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user =User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ユーザー情報を更新しました。"
+      redirect_to admin_user_path(@user)
+    else 
+      render :edit 
+    end 
+  end 
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :is_active)
+  end 
+  
 end
