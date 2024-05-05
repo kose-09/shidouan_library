86ec6b591968702239bf717cde93a495a67e3039
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-05-05 17:39:07 +0000

    [Update] 管理者用のコメント管理機能にアクセス制限を追加

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,7 +1,13 @@
 class Admin::PostCommentsController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
+    @post = Post.find(params[:post_id])
   end 
   
   def destroy
+    post_comment = PostComment.find(params[:id])
+    post_comment.destroy
+    redirect_to admin_post_post_comments_path(:post_id)
   end 
 end
