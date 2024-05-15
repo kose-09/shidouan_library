b03073a3987ca422ff3880f942544bc288f3f937
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-05-15 19:07:04 +0000

    [Update] 管理者用の投稿一覧画面のレイアウトを作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,7 +2,7 @@ class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-    @posts = Post.all
+    @posts = Post.all.page(params[:page]).per(10)
   end 
   
   def show
@@ -18,4 +18,9 @@ class Admin::PostsController < ApplicationController
   def post_comments
     @post = Post.find(params[:id])
   end 
+  
+  def search
+    @post = Post.search(params[:keyword], params[:subject_category_id]).page(params[:page]).per(10)
+  end
+  
 end
