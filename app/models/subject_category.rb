a52e13d1c0a4747611b1459d8d2d7e6595b2a6a2
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-07-20 14:48:45 +0000

    [Add] SubjectCategoriesモデルにuniquenessバリデーションを追記

--- app/models/subject_category.rb
+++ app/models/subject_category.rb
@@ -2,4 +2,5 @@ class SubjectCategory < ApplicationRecord
   has_many :posts, dependent: :nullify
   validates :name, presence: true
   validates :name, length: { maximum: 10 }
+  validates :name, uniqueness: true
 end
